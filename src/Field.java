1302627a1eb4086b1b72007974b0bc2832c0a07c
Dorottya Julianna Ács <acsdoro2005@gmail.com>
2024-11-15 13:36:39 +0100

    window works

---
+++ src/Field.java
@@ -0,0 +1,12 @@
+public class Field {
+    private boolean filled;
+    public Field() {
+        filled = false;
+    }
+    public boolean isFilled() {
+        return filled;
+    }
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+}
---
+++ src/Grid.java
@@ -0,0 +1,6 @@
+public class Grid {
+    Field[][] grid;
+    public Grid(int rows, int cols) {
+        grid = new Field[rows][cols];
+    }
+}
