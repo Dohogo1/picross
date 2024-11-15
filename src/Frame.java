aea92f9f78d167a879802f4c782749e8cc1f3cd3
Dorottya Julianna Ács <acsdoro2005@gmail.com>
2024-11-15 13:40:27 +0100

    asdasd

--- src/Frame.java
+++ src/Frame.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-public class Frame {
+public class Frame extends JFrame {
+    public Frame() {
+        setTitle("Picross");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Frame();
+        System.out.println("asdasd");
+    }
+
+
 }
